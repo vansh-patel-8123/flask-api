28d55e9c88eefc893a96c7d3ec16eb48117ada7f
vansh patel <vanshpatel1299@gmail.com>
2023-09-24 13:30:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,45 @@
 from flask import Flask, request, jsonify
 import yfinance as yf
 
-app = Flask(__name__)
+app = Flask(_name_)
 
+# Dictionary mapping ticker symbols to their total revenue values (a list of 5 values)
+total_revenues_dict = {
+    "AAPL": [294135000000, 265595000000, 260174000000, 247534000000, 215639000000],
+    "TSLA": [31540000000, 24076000000, 21940000000, 21706500000, 11075400000],
+    "GOOGL": [182527000000, 161857000000, 136819000000, 110855000000, 90119000000],
+    "AMZN": [386064000000, 280522000000, 232887000000, 177866000000, 135987000000],
+    "MSFT": [168088000000, 143015000000, 125843000000, 110360000000, 89953000000],
+    "FB": [92903000000, 70400000000, 55838000000, 40653000000, 27638000000],
+    "NVDA": [168144000000, 109181000000, 91118000000, 67893000000, 49710000000],
+    "JPM": [115627000000, 106506000000, 105486000000, 111445000000, 95620000000],
+    "WMT": [559151000000, 555188000000, 500343000000, 485873000000, 482130000000],
+    # You can add more companies here
+}
 
-@app.route("/get_stock_info", methods=["GET"])
-def get_stock_info():
+@app.route("/get_data", methods=["GET"])
+def get_data():
     ticker = request.args.get("ticker")
 
     if not ticker:
         return jsonify({"error": "Ticker symbol is required"}), 400
 
+    result = {"ticker": ticker}
+
+    # Check if the ticker symbol exists in the dictionary
+    if ticker in total_revenues_dict:
+        revenues = total_revenues_dict[ticker]
+        result["revenues"] = revenues
+
     try:
         stock = yf.Ticker(ticker)
+
+        # Fetch historical stock prices for the specified period (default: 1 day)
+        history = stock.history(period="1d")
+
+        # Extract date and closing price values
+        price_data = history[["Open"]].reset_index()
+        result["stock_price"] = price_data.to_dict(orient="records")
 
         # Retrieve the latest values for EPS, PE ratio, and Price-to-Book ratio
         info = stock.info
@@ -20,81 +47,14 @@ def get_stock_info():
         pe_ratio = info.get("trailingPE", None)  # Price-to-Earnings ratio
         pb_ratio = info.get("priceToBook", None)  # Price-to-Book ratio
 
-        # Retrieve the latest dividends data
-        # print(stock.dividends)
-        # dividends = stock.dividends.tail(1).reset_index().to_dict(orient="records")
-
-        result = {
-            "ticker": ticker,
-            "eps": eps,
-            "pe_ratio": pe_ratio,
-            "priceToBook": pb_ratio,
-        }
+        result["eps"] = eps
+        result["pe_ratio"] = pe_ratio
+        result["priceToBook"] = pb_ratio
 
         return jsonify(result)
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
-@app.route("/get_revenues", methods=["GET"])
-def get_revenues():
-    ticker = request.args.get("ticker")
-    period = request.args.get("period", "3y")  # Default period is 3 years
-
-    if not ticker:
-        return jsonify({"error": "Ticker symbol is required"}), 400
-
-    try:
-        stock = yf.Ticker(ticker)
-
-        # Fetch historical data for the specified period
-        history = stock.history(period=period)
-        print(history)
-
-        n_years = int(period[:-1])  # Extract the number of years from the period
-
-        # Calculate total revenue for each year
-        total_revenues = [
-            history.iloc[-n:]["Open"].sum() for n in range(1, n_years + 1)
-        ]
-
-        result = {
-            "ticker": ticker,
-            "total_revenues": total_revenues,
-        }
-
-        return jsonify(result)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-
-@app.route("/get_stock_price", methods=["GET"])
-def get_stock_price():
-    ticker = request.args.get("ticker")
-    period = request.args.get("period", "1d")  # Default period is 1 day
-
-    if not ticker:
-        return jsonify({"error": "Ticker symbol is required"}), 400
-
-    try:
-        stock = yf.Ticker(ticker)
-
-        # Fetch historical data for the specified period
-        history = stock.history(period=period)
-
-        # Extract date and closing price values
-        price_data = history[["Open"]].reset_index()
-
-        result = {
-            "ticker": ticker,
-            "period": period,
-            "price_data": price_data.to_dict(orient="records"),
-        }
-
-        return jsonify(result)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-
-if __name__ == "__main__":
+if _name_ == "_main_":
     app.run(debug=True)
