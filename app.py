12d3efca374cd7282f0389ae559c0d63514cf219
vansh patel <vanshpatel1299@gmail.com>
2023-09-24 14:06:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,75 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Dictionary mapping ticker symbols to their total revenue values (a list of 5 values)
+# Dictionary mapping ticker symbols to their total revenue values (a list of dictionaries with year and revenue)
 total_revenues_dict = {
-    "AAPL": [294135000000, 265595000000, 260174000000, 247534000000, 215639000000],
-    "TSLA": [31540000000, 24076000000, 21940000000, 21706500000, 11075400000],
-    "GOOGL": [182527000000, 161857000000, 136819000000, 110855000000, 90119000000],
-    "AMZN": [386064000000, 280522000000, 232887000000, 177866000000, 135987000000],
-    "MSFT": [168088000000, 143015000000, 125843000000, 110360000000, 89953000000],
-    "FB": [92903000000, 70400000000, 55838000000, 40653000000, 27638000000],
-    "NVDA": [168144000000, 109181000000, 91118000000, 67893000000, 49710000000],
-    "JPM": [115627000000, 106506000000, 105486000000, 111445000000, 95620000000],
-    "WMT": [559151000000, 555188000000, 500343000000, 485873000000, 482130000000],
-    # You can add more companies here
+    "AAPL": [
+        {"year": 2023, "revenue": 294135000000},
+        {"year": 2022, "revenue": 265595000000},
+        {"year": 2021, "revenue": 260174000000},
+        {"year": 2020, "revenue": 247534000000},
+        {"year": 2019, "revenue": 215639000000},
+    ],
+    "TSLA": [
+        {"year": 2023, "revenue": 31540000000},
+        {"year": 2022, "revenue": 24076000000},
+        {"year": 2021, "revenue": 21940000000},
+        {"year": 2020, "revenue": 21706500000},
+        {"year": 2019, "revenue": 11075400000},
+    ],
+    "GOOGL": [
+        {"year": 2023, "revenue": 182527000000},
+        {"year": 2022, "revenue": 161857000000},
+        {"year": 2021, "revenue": 136819000000},
+        {"year": 2020, "revenue": 110855000000},
+        {"year": 2019, "revenue": 90119000000},
+    ],
+    # Add more companies with year and revenue data here
+}
+
+# Dictionary mapping ticker symbols to their stock info
+stock_info_dict = {
+    "AAPL": {"eps": 4.51, "pe_ratio": 32.55, "priceToBook": 10.68},
+    "TSLA": {"eps": 0.01, "pe_ratio": 123.45, "priceToBook": 56.78},
+    "GOOGL": {"eps": 29.73, "pe_ratio": 28.12, "priceToBook": 6.54},
+    # Add more stock info data as needed for additional companies
+}
+
+# Dictionary mapping ticker symbols to their latest stock price (realistic hardcoded values)
+latest_stock_price_dict = {
+    "AAPL": 150.0,
+    "TSLA": 750.0,
+    "GOOGL": 2800.0,
+    # Add more latest stock prices here
+}
+
+# Realistic hardcoded values for stock price (10 data points)
+stock_price_dict = {
+    "AAPL": [
+        {"Date": "2023-09-20", "Open": 150.0},
+        {"Date": "2023-09-21", "Open": 155.0},
+        {"Date": "2023-09-22", "Open": 160.0},
+        {"Date": "2023-09-23", "Open": 158.0},
+        {"Date": "2023-09-24", "Open": 157.5},
+        {"Date": "2023-09-27", "Open": 159.0},
+        {"Date": "2023-09-28", "Open": 161.0},
+        {"Date": "2023-09-29", "Open": 162.0},
+        {"Date": "2023-09-30", "Open": 165.0},
+        {"Date": "2023-10-01", "Open": 166.0},
+    ],
+    "TSLA": [
+        {"Date": "2023-09-20", "Open": 750.0},
+        {"Date": "2023-09-21", "Open": 755.0},
+        {"Date": "2023-09-22", "Open": 760.0},
+        {"Date": "2023-09-23", "Open": 758.0},
+        {"Date": "2023-09-24", "Open": 757.5},
+        {"Date": "2023-09-27", "Open": 759.0},
+        {"Date": "2023-09-28", "Open": 761.0},
+        {"Date": "2023-09-29", "Open": 762.0},
+        {"Date": "2023-09-30", "Open": 765.0},
+        {"Date": "2023-10-01", "Open": 766.0},
+    ],
+    # Add more stock price data for other companies here
 }
 
 @app.route("/get_data", methods=["GET"])
@@ -32,23 +89,20 @@ def get_data():
     else:
         return jsonify({"error": "Ticker symbol not found"}), 404
 
-    # Realistic hardcoded values for stock price and stock info (replace with real values)
-    stock_price = [
-        {"Date": "2023-09-20", "Open": 150.0},
-        {"Date": "2023-09-21", "Open": 155.0},
-        {"Date": "2023-09-22", "Open": 160.0},
-        # Add more data points as needed
-    ]
+    # Fetch the latest stock price from the dictionary
+    if ticker in latest_stock_price_dict:
+        latest_price = latest_stock_price_dict[ticker]
+        result["baseValue"] = latest_price
 
-    stock_info = {
-        "eps": 4.51,
-        "pe_ratio": 32.55,
-        "priceToBook": 10.68,
-        # Add more stock info data as needed
-    }
+    # Retrieve stock info from the dictionary
+    if ticker in stock_info_dict:
+        stock_info = stock_info_dict[ticker]
+        result["stock_info"] = stock_info
 
-    result["stock_price"] = stock_price
-    result["stock_info"] = stock_info
+    # Add the list of stock prices to the response
+    if ticker in stock_price_dict:
+        stock_prices = stock_price_dict[ticker]
+        result["stock_price"] = stock_prices
 
     return jsonify(result)
 
